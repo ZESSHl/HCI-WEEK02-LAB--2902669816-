50913bc6200121a7da6077852110b8671cf1df3a
ZESSHl <koojefferson@gmail.com>
2025-09-27 17:57:36 +0700

    Keep only multipage-html folder

--- Assignment_HCI_1/Addition-Substraction_calc.py
+++
@@ -1,5 +0,0 @@
-num1 = float(input("Input First Number: "))
-num2 = float(input("Input Second Number: "))
-
-print("Addition:"    , num1 + num2)
-print("Substraction:", num1 - num2)
--- Assignment_HCI_1/Multiplication-Division_calc.py
+++
@@ -1,7 +0,0 @@
-num1 = float(input("Input First Number: "))
-num2 = float(input("Input Second Number: "))
-
-x = num1 * num2
-y = num1 / num2
-print("Multiplication:", round(x, 4))
-print("Division:", round(y, 4))
